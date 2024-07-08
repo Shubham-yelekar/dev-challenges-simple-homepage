fd1b7f38214e19ca6c06558d33fc285b97e30e5f
shubham <shubhamyelekar0@gmail.com>
2024-07-08 18:09:30 +0530

    hmaburger

--- js/script.js
+++ js/script.js
@@ -1,2 +1,12 @@
 // Add some interactivity to the website
 console.log("Hello, world!");
+
+const menuBtn =  document.querySelector('.mobile-menu')
+const menuBar = document.querySelectorAll('.bar')
+
+function toggleMenu(){
+
+}
+
+
+
