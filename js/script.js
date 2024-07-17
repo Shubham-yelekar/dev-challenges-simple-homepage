afa0a77ec30aa06a1bb4ff095ce2c5ce8b25ea34
shubham <shubhamyelekar0@gmail.com>
2024-07-17 15:05:22 +0530

    new build

--- js/script.js
+++ js/script.js
@@ -4,8 +4,24 @@ console.log("Hello, world!");
 const menuBtn =  document.querySelector('.mobile-menu')
 const menuBar = document.querySelectorAll('.bar')
 
+const logoLight = document.querySelector('.logo-light')
+const logoDark = document.querySelector('.logo-dark')
+
 function toggleMenu(){
 
+}
+
+let defaultMode = 'Light'
+
+function toggleDark(){
+  if (defaultMode === 'Light'){
+
+    defaultMode = "Dark"
+    console.log(defaultMode)
+  } else if (defaultMode === 'Dark'){
+    defaultMode = "Light"
+    console.log(defaultMode)
+  }
 }
 
 
